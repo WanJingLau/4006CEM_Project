b07d2f5903e056607e617bc25cf6b2d7bcbb3112
WanJing923 <p20012339@student.newinti.edu.my>
2021-10-09 17:19:33 +0800

    blablabla db

--- edit_profile.py
+++ edit_profile.py
@@ -2,7 +2,6 @@ from tkinter import *
 from PIL import Image, ImageTk
 from change_password import change_password
 from change_username import change_username
-from db_conn import readFromDb, insertUpdateDeleteToDb
 
 def edit_profile():
     global edit_profile_screen
@@ -23,13 +22,3 @@ def edit_profile():
  
     edit_profile_screen.title(txt_edit_profile)
     edit_profile_screen.geometry(geometry_size)
-
-def page_not_found():
-    global page_not_found_screen
-    page_not_found_screen = Toplevel(edit_profile_screen)
-    page_not_found_screen.title("Error")
-    Label(page_not_found_screen, text="Page not found").pack()
-    Button(page_not_found_screen, text="OK", command=delete_page_not_found).pack()
-
-def delete_page_not_found():
-    page_not_found_screen.destroy() 
