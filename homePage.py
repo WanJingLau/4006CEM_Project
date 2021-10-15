53d14ac316b44eccc9217a88b8b553f39a757f30
OhWenChi <83630382+OhWenChi@users.noreply.github.com>
2021-10-15 21:23:19 +0800

    homepage_changes

--- homePage.py
+++ homePage.py
@@ -1,6 +1,8 @@
+from check_in_days import check_in_days
 from delete_ebooks import delete_ebooks
 from edit_ebooks import edit_ebooks
 from edit_profile import edit_profile
+from help_center import help_center
 from settings import settings
 from tkinter import *
 from tkinter.constants import N
@@ -17,12 +19,16 @@ def homepage(email):
     global store_ebooks_icon
     global upload_ebooks_icon
     global edit_profile_icon
+    global help_center_icon
+    global check_in_days_icon
     homepage_screen = Toplevel()
     homepage_icon = ImageTk.PhotoImage(Image.open("homepage.png").resize((80, 80), Image.ANTIALIAS))
     book_categories_icon = ImageTk.PhotoImage(Image.open("bookcategories.png").resize((50, 50), Image.ANTIALIAS))
     store_ebooks_icon = ImageTk.PhotoImage(Image.open("storebooks.png").resize((50, 50), Image.ANTIALIAS))
     upload_ebooks_icon = ImageTk.PhotoImage(Image.open("uploadbooks.png").resize((50, 50), Image.ANTIALIAS))
     edit_profile_icon = ImageTk.PhotoImage(Image.open("profile.png").resize((50, 50), Image.ANTIALIAS))
+    help_center_icon = ImageTk.PhotoImage(Image.open("helpcenter.png").resize((50, 50), Image.ANTIALIAS))
+    check_in_days_icon = ImageTk.PhotoImage(Image.open("checkin.png").resize((50, 50), Image.ANTIALIAS))
     #text variable declaration
     txt_homepage = "Home Page"
     txt_logout = "Log Out"
@@ -31,6 +37,8 @@ def homepage(email):
     txt_store_books = "Favourite Books"
     txt_upload_books = "Upload E-Books"
     txt_edit_profile = "Edit Profile"
+    txt_help_center = "Help Center"
+    txt_check_in_days = "Check In Days"
     #homepage title, size, maximize window
     homepage_screen.title(txt_homepage)
     homepage_screen.state("zoomed")
@@ -52,6 +60,12 @@ def homepage(email):
     #upload ebook
     Label(homepage_screen, image = upload_ebooks_icon).place(x=550, y=330)
     Button(homepage_screen, text=txt_upload_books, font = ("Helvetica", 12, "bold"), foreground="black", width=16, height=1, cursor="hand2", command = upload_ebooks).place(x=650,y=340)
+    #help center
+    Label(homepage_screen, image = help_center_icon).place(x=80, y=430)
+    Button(homepage_screen, text=txt_help_center, font = ("Helvetica", 12, "bold"), foreground="black", width=16, height=1, cursor="hand2", command = help_center).place(x=180,y=440)
+    #check in days
+    Label(homepage_screen, image = check_in_days_icon).place(x=550, y=430)
+    Button(homepage_screen, text=txt_check_in_days, font = ("Helvetica", 12, "bold"), foreground="black", width=16, height=1, cursor="hand2", command = check_in_days).place(x=650,y=440)
 
     #check if is admin, show edit & delete ebook
     show_lbl(email)
@@ -73,10 +87,16 @@ def show_lbl(email):
     result = readFromDb(dbQuery)
     global editebooks_icon
     global deleteebooks_icon
+    global help_center_icon
+    global check_in_days_icon
     txt_edit_ebooks = "Edit E-Books"
     txt_delete_ebooks = "Delete E-Books"
+    txt_help_center = "Help Center"
+    txt_check_in_days = "Check In Days"
     editebooks_icon = ImageTk.PhotoImage(Image.open("editebooks.png").resize((50, 50), Image.ANTIALIAS))
     deleteebooks_icon = ImageTk.PhotoImage(Image.open("deleteebooks.png").resize((50, 50), Image.ANTIALIAS))
+    help_center_icon = ImageTk.PhotoImage(Image.open("helpcenter.png").resize((50, 50), Image.ANTIALIAS))
+    check_in_days_icon = ImageTk.PhotoImage(Image.open("checkin.png").resize((50, 50), Image.ANTIALIAS))
     if result[0] == "admin":
         #edit ebook
         Label(homepage_screen, image = editebooks_icon).place(x=80, y=430)
@@ -84,6 +104,12 @@ def show_lbl(email):
         #delete ebook
         Label(homepage_screen, image = deleteebooks_icon).place(x=550, y=430)
         Button(homepage_screen, text = txt_delete_ebooks, font = ("Helvetica", 12, "bold"), foreground="black", width=16, height=1, cursor="hand2", command = delete_ebooks).place(x=650,y=440)
+        #help center
+        Label(homepage_screen, image = help_center_icon).place(x=80, y=530)
+        Button(homepage_screen, text=txt_help_center, font = ("Helvetica", 12, "bold"), foreground="black", width=16, height=1, cursor="hand2", command = help_center).place(x=180,y=540)
+        #check in days
+        Label(homepage_screen, image = check_in_days_icon).place(x=550, y=530)
+        Button(homepage_screen, text=txt_check_in_days, font = ("Helvetica", 12, "bold"), foreground="black", width=16, height=1, cursor="hand2", command = check_in_days).place(x=650,y=540)
     Label(homepage_screen, text = "Hi, " + result[1] + ". Welcome back!", font = ("Helvetica", 12, "bold")).place(x=80, y=150)
 def delete_page_not_found():
     page_not_found_screen.destroy()
